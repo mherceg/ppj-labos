a5ea58776ecf7736c107f4cfb36a5e55fbd6b914
Adriano Bacac <adrianobacac@gmail.com>
2014-10-18 16:11:01 +0200

    citanje xmla sa jabx

--- GLA/S.java
+++ GLA/S.java
@@ -1,5 +1,17 @@
+import java.io.StringReader;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
 
 public class S implements Snjeguljica {
+	private Definicija definicija;
+	S() throws JAXBException{
+		JAXBContext context = JAXBContext.newInstance(Definicija.class);
+		Unmarshaller um = context.createUnmarshaller();
+		definicija = (Definicija) um.unmarshal(new StringReader("Definicija.xml"));
+	}
 	public Patuljak prelazim(String stanje,String ucitano){
 		return null;
 	}
