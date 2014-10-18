95a30afd78b7450466b8bbb0d5d86e72b28927a3
Adriano Bacac <adrianobacac@gmail.com>
2014-10-18 17:00:23 +0200

    getters, setters, privatne varijable

--- GLA/analizator/Akcija.java
+++ GLA/analizator/Akcija.java
@@ -1,8 +1,43 @@
 
 public class Akcija {
-	public String regex;
-	public String identifikator;
-	public boolean noviRed;
-	public int vraca;
-	public String novoStanje;
+	private String regex;
+	private String identifikator;
+	private boolean noviRed;
+	private int vraca;
+	private String novoStanje;
+	
+	
+	
+	public String getRegex() {
+		return regex;
+	}
+	public void setRegex(String regex) {
+		this.regex = regex;
+	}
+	public String getIdentifikator() {
+		return identifikator;
+	}
+	public void setIdentifikator(String identifikator) {
+		this.identifikator = identifikator;
+	}
+	public boolean isNoviRed() {
+		return noviRed;
+	}
+	public void setNoviRed(boolean noviRed) {
+		this.noviRed = noviRed;
+	}
+	public int getVraca() {
+		return vraca;
+	}
+	public void setVraca(int vraca) {
+		this.vraca = vraca;
+	}
+	public String getNovoStanje() {
+		return novoStanje;
+	}
+	public void setNovoStanje(String novoStanje) {
+		this.novoStanje = novoStanje;
+	}
+	
+	
 }
--- GLA/analizator/Stanje.java
+++ GLA/analizator/Stanje.java
@@ -2,6 +2,20 @@ import java.util.List;
 
 
 public class Stanje {
-	String ime;
-	List<Akcija> akcija;
+	private String ime;
+	private List<Akcija> akcija;
+	
+	
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public List<Akcija> getAkcija() {
+		return akcija;
+	}
+	public void setAkcija(List<Akcija> akcija) {
+		this.akcija = akcija;
+	}
 }
