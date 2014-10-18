75f154c920a4fae9406f60f1e0a29b3d63c5c6b1
Luka Cvetković <l.cvetk@gmail.com>
2014-10-18 16:57:19 +0200

    Alpha version

--- GLA/analizator/LA.java
+++ GLA/analizator/LA.java
@@ -2,100 +2,167 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.ProcessBuilder.Redirect;
 
+import javax.xml.bind.JAXBException;
 
 public class LA {
-	public static int pocetak=0,zadnjeNadeno=0,trenutni=0;
-	Patuljak zadnjiNeNullPatuljak;
+	public static int pocetak = 0, zadnjeNadeno = 0, trenutni = 0;
+	public static Patuljak zadnjiNeNullPatuljak;
+	public static int red = 1;
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Run LA");
-		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		int red=1;
-		String niz="";
-		String source="";
-		Patuljak zadnjiNeNull;
+		/*
+		 * Inicijalizacija pocetnih varijabli
+		 */
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
+
+		String niz = "";
+		String source = "";
+		Patuljak patuljak;
 		String stanje;
-		Snjeguljica snjeguljica;
-		
-		stanje=snjeguljica.getPocetno();
-				
-		
+		Snjeguljica snjeguljica = null;
+		try {
+			snjeguljica = new S();
+		} catch (JAXBException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		/*
+		 * Pocetno stanje dobivamo iz snjeguljice i trebalo bi bit pocetno
+		 */
+		stanje = snjeguljica.getPocetno();
+
 		// Compile Snjeguljice i onda konstruktor snjeguljice
 		compile("Snjeguljica.java");
-		
-		
+
+		/*
+		 * Ucitavanje s stdina i radenje ogromnog stringa u kojem je sve
+		 */
 		try {
-			while((niz = reader.readLine()) != null){
-				source+=niz;
-				source+="/n";
-				
+			while ((niz = reader.readLine()) != null) {
+				source += niz;
+				source += "/n";
+
 			}
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		}		
-		
-		
-		try{
-			reader.close();
 		}
-		catch(Exception e){
+
+		try {
+			reader.close();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
-	
-		
-		
+
+		/*
+		 * Slanje snjeguljici znakove jedan po jedan da ona matcha i nase
+		 * trenutno stanje koje onda kasnije kroz patuljka gledam
+		 */
+		while (pocetak < source.length()) {
+			// Postavljamo neNullPatuljka na null te idemo ispocetka svaki put
+			zadnjiNeNullPatuljak = null;
+			// Idemo for petlju po svakom substringu u nizu
+			for (int i = pocetak; i < source.length(); i++) {
+				patuljak = snjeguljica.prelazim(
+						source.substring(pocetak, i + 1), stanje);
+				obradiPatuljka(patuljak, i + 1);
+
+			}
+			// Ako nije nista matchao do kraja ulaza onda je greska i ispisuje
+			// se znak
+			if (zadnjiNeNullPatuljak == null) {
+				ispisiGresku((source.substring(pocetak, 1)), red);
+			}
+			// Ako je nasao neki povoljni match onda ga ispise i idemo dalje
+			else {
+				ispisiIzlaz(source.substring(pocetak, zadnjeNadeno), red);
+				pocetak = pocetak + zadnjeNadeno;
+			}
+		}
 	}
+
+	/**
+	 * Uzima parametar patuljka i znak do kojeg je dosao te gleda da li je ista
+	 * matchano, ako je onda pamti string do kojeg je matcha, a ako nije matchan
+	 * se vraca i ide dalje. Gleda da li je patuljak poslao da se doslo u novi
+	 * red, ako je povecava globalni brojac redaka i pamti
+	 * 
+	 * @param patuljak
+	 *            koji nam je dosao sa informacijama
+	 * @param i
+	 *            je substring do kojeg je dosao od pocetka
+	 */
+	private static void obradiPatuljka(Patuljak patuljak, int i) {
+		// Ako je patuljak null onda se samo vraca i nije matchao ni jednu
+		// leksicku jedinku
+		if (patuljak == null) {
+			return;
+		} else { // ako je nasao nesto onda pamti tog patuljka i pamti substring
+			zadnjiNeNullPatuljak = patuljak;
+			zadnjeNadeno = i;
+			//povecava brojac reda ako je potrebno
+			if (patuljak.getNoviRed() == true) {
+				red++;
+
+			}
+
+		}
+
+	}
+
 	/**
 	 * ispisivanje leksièkih jedinki
-	 * @param izvorniTekst tekst koji je kljucna rijec za odredenu jedinku
+	 * 
+	 * @param izvorniTekst
+	 *            tekst koji je kljucna rijec za odredenu jedinku
 	 * @param red
 	 */
-	public void ispisiIzlaz(String izvorniTekst, int red){
-		//ispisujem string od pocetka do zadnjeg nadenog
-		String tekstZaIspis=izvorniTekst.substring(pocetak,zadnjeNadeno);
-		
-		String uniformniZnak=zadnjiNeNullPatuljak.getNasao();
-		
-		System.out.println(uniformniZnak+" "+red+" "+tekstZaIspis);
-		
-		
-		
+	public static void ispisiIzlaz(String izvorniTekst, int red) {
+		// ispisujem string od pocetka do zadnjeg nadenog
+		String tekstZaIspis = izvorniTekst.substring(pocetak, zadnjeNadeno);
+
+		String uniformniZnak = zadnjiNeNullPatuljak.getNasao();
+
+		System.out.println(uniformniZnak + " " + red + " " + tekstZaIspis);
+
 	}
+
 	/**
 	 * Ispisivanje greške
-	 * @param greska je greška u kodu
-	 * @param red je linija reda
+	 * 
+	 * @param greska
+	 *            je greška u kodu
+	 * @param red
+	 *            je linija reda
 	 */
-	public void ispisiGresku(String greska, int red){
-		
-		System.err.println("ERROR " + red + " "+ greska);
-		
+	public static void ispisiGresku(String greska, int red) {
+
+		System.err.println("ERROR " + red + " " + greska);
+
 	}
-	
-	private static void compile(String className){
-		try{
-		Process pro = Runtime.getRuntime().exec(className);
-		printLines(className + " stdout:", pro.getInputStream());
-	    printLines(className + " stderr:", pro.getErrorStream());
-		}
-		catch(Exception e){
+
+	private static void compile(String className) {
+		try {
+			Process pro = Runtime.getRuntime().exec(className);
+			printLines(className + " stdout:", pro.getInputStream());
+			printLines(className + " stderr:", pro.getErrorStream());
+		} catch (Exception e) {
 			System.out.println("Greska kod compileanja");
 			e.printStackTrace();
 		}
 	}
-	
-	private static void printLines(String name, InputStream ins) throws Exception {
-	    String line = null;
-	    BufferedReader in = new BufferedReader(
-	        new InputStreamReader(ins));
-	    while ((line = in.readLine()) != null) {
-	        System.out.println(name + " " + line);
-	    }
-	  }
+
+	private static void printLines(String name, InputStream ins)
+			throws Exception {
+		String line = null;
+		BufferedReader in = new BufferedReader(new InputStreamReader(ins));
+		while ((line = in.readLine()) != null) {
+			System.out.println(name + " " + line);
+		}
+	}
 }
