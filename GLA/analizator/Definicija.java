111927a04d8f7f14bd383aa7e1cb6cd595ed558a
Adriano Bacac <adrianobacac@gmail.com>
2014-10-18 18:57:03 +0200

    dodano pocetno stanje

--- GLA/analizator/Definicija.java
+++ GLA/analizator/Definicija.java
@@ -7,7 +7,8 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name="Definicija")
 public class Definicija {
 	private List<Stanje> stanje;
-
+	private String pocetno;
+	
 	Definicija(){
 		stanje = new ArrayList<>();
 	}
@@ -18,6 +19,15 @@ public class Definicija {
 	@XmlElement(name="Stanje")
 	public void setStanje(List<Stanje> stanje) {
 		this.stanje = stanje;
+	}
+
+	public String getPocetno() {
+		return pocetno;
+	}
+	
+	@XmlElement(name="pocetno")
+	public void setPocetno(String pocetno) {
+		this.pocetno = pocetno;
 	}
 	
 }
