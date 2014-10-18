f3e378d07c288e9964a9a0d7aad67e38e22cba03
Adriano Bacac <adrianobacac@gmail.com>
2014-10-18 16:18:21 +0200

    prebaceno sve sto je potrebo za P i S u LA

--- GLA/analizator/S.java
+++ GLA/analizator/S.java
@@ -7,12 +7,18 @@ import javax.xml.bind.Unmarshaller;
 
 public class S implements Snjeguljica {
 	private Definicija definicija;
+	
+	
 	S() throws JAXBException{
 		JAXBContext context = JAXBContext.newInstance(Definicija.class);
 		Unmarshaller um = context.createUnmarshaller();
 		definicija = (Definicija) um.unmarshal(new StringReader("Definicija.xml"));
 	}
+	
+	
+	
 	public Patuljak prelazim(String stanje,String ucitano){
+		
 		return null;
 	}
 
