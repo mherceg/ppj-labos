9852d36adcd3aef73914125920cb03897b665859
Mihael Francekovic <mihael.francekovic@fer.hr>
2014-10-18 14:10:57 +0200

    Add reader

--- GLA/GLA.java
+++ GLA/GLA.java
@@ -1,9 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 
 public class GLA {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
--- GLA/analizator/LA.java
+++ GLA/analizator/LA.java
@@ -1,10 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 
 public class LA {
 
 	public static void main(String[] args) {
-				/**
-				 * komentar
-				 */
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		
+		
+		
+		try{
+			reader.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 }
