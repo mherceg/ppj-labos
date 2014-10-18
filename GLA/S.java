795029b9bb9f40aafab8f5deba711639d4152824
Matija Herceg <herc@gmail.com>
2014-10-18 16:07:15 +0200

    fixed interface snjeguljica

--- GLA/S.java
+++ GLA/S.java
@@ -3,4 +3,9 @@ public class S implements Snjeguljica {
 	public Patuljak prelazim(String stanje,String ucitano){
 		return null;
 	}
+
+	@Override
+	public String getPocetno() {
+		return null;
+	}
 }
