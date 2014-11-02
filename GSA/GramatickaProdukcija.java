dc2a6957fbada2c68ccd484aad3b662a5bc4edde
Luka Cvetković <l.cvetk@gmail.com>
2014-11-02 14:10:00 +0100

    dodana GramProd

---
+++ GSA/GramatickaProdukcija.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+public class GramatickaProdukcija {
+	private String lijevaStrana;
+	private List<String> desnaStrana;
+
+	public GramatickaProdukcija(String lijevaStrana) {
+
+		this.lijevaStrana = lijevaStrana;
+	}
+
+	public GramatickaProdukcija() {
+
+	}
+
+	public String getLijevaStrana() {
+		return lijevaStrana;
+	}
+
+	public void setLijevaStrana(String lijevaStrana) {
+		this.lijevaStrana = lijevaStrana;
+	}
+
+	public List<String> getDesnaStrana() {
+		return desnaStrana;
+	}
+
+	public void setDesnaStrana(List<String> desnaStrana) {
+		this.desnaStrana = desnaStrana;
+	}
+
+	public void dodajNoviDesniIzraz(String izraz){
+		desnaStrana.add(izraz);
+	}
+	
+}
