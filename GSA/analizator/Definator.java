d78568f25fc0b4ccc04d547c5e21dfe625140aa2
Luka Cvetković <l.cvetk@gmail.com>
2014-11-02 13:20:25 +0100

    Dodan definator koji ce imat zaglavlje datoteke

---
+++ GSA/analizator/Definator.java
@@ -0,0 +1,25 @@
+package analizator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Definator {
+	private List<String> zavrsniZnakovi = new ArrayList<String>();
+	private List<String> nezavrsniZnakovi = new ArrayList<String>();
+	private List<String> sinkronizacijskiZnakovi = new ArrayList<String>();
+
+	public void dodajZavrsnuListu(List list) {
+		zavrsniZnakovi=list;
+	}
+
+	public void dodajNezavrsnuListu(List list) {
+		nezavrsniZnakovi=list;
+	}
+
+	public void dodajSinkronizacijskuListu(List list) {
+		sinkronizacijskiZnakovi=list;
+	}
+	
+	
+
+}
