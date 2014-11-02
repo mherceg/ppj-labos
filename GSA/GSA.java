820b20d584ea4fa760ecf35d3ffedd1221cffdcb
Mihael Francekovic <mihael.francekovic@fer.hr>
2014-11-02 15:32:00 +0100

    Ucitavanje fix sa duplicima

--- GSA/GSA.java
+++ GSA/GSA.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class GSA {	
@@ -13,7 +14,7 @@ public class GSA {
 		String line;
 		Automat eNKA=new Automat();
 		
-		List<GramatickaProdukcija> listaGramtickihProdukcija = new ArrayList<GramatickaProdukcija>();
+		List<GramatickaProdukcija> listaGramtickihProdukcija = new LinkedList<GramatickaProdukcija>();
 		
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -25,14 +26,15 @@ public class GSA {
 			e.printStackTrace();
 		}
 		
-		/*System.out.println(definator.getNezavrsniZnakovi().toString());
+		System.out.println(definator.getNezavrsniZnakovi().toString());
 		System.out.println(definator.getZavrsniZnakovi().toString());
 		System.out.println(definator.getSinkronizacijskiZnakovi().toString());
 		
-		System.out.println();*/
+		System.out.println();
 		
 		List<String> vecPunjeniNezavrsniZnakovi = new ArrayList<String>();
 		GramatickaProdukcija tempGramtickaProdukcija = new GramatickaProdukcija();
+		boolean dodaj = true;
 				
 		try {
 			line = reader.readLine();
@@ -40,9 +42,11 @@ public class GSA {
 				if(line.startsWith("<")){
 					tempGramtickaProdukcija = new GramatickaProdukcija();
 					if(vecPunjeniNezavrsniZnakovi.contains(line)){
+						dodaj = false;
 						tempGramtickaProdukcija = listaGramtickihProdukcija.get(getIndex(line, listaGramtickihProdukcija));
 					}
 					else{
+						dodaj = true;
 						tempGramtickaProdukcija.setLijevaStrana(line);
 						vecPunjeniNezavrsniZnakovi.add(line);						
 					}
@@ -56,14 +60,16 @@ public class GSA {
 							break;
 						}
 					}
-					listaGramtickihProdukcija.add(tempGramtickaProdukcija);
+					if(dodaj){
+						listaGramtickihProdukcija.add(tempGramtickaProdukcija);
+					}
 				}
 			}
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
 		
-		/*for(GramatickaProdukcija gramatickaProdukcija : listaGramtickihProdukcija){
+		for(GramatickaProdukcija gramatickaProdukcija : listaGramtickihProdukcija){
 			System.out.print(gramatickaProdukcija.getLijevaStrana());
 			System.out.print(" -> ");
 			List<String> desneStrane = gramatickaProdukcija.getDesnaStrana();
@@ -71,7 +77,7 @@ public class GSA {
 				System.out.print(s + "|");
 			}
 			System.out.println();			
-		}*/
+		}
 		
 		
 		
@@ -91,7 +97,6 @@ public class GSA {
 		int i = 0;
 		System.out.println(list.size());
 		for(GramatickaProdukcija gramatickaProdukcija : list){
-			System.out.println("usporedujem -> " + gramatickaProdukcija.getLijevaStrana() + " i " + string);
 			if(gramatickaProdukcija.getLijevaStrana().equals(string)){				
 				return i;
 			}
