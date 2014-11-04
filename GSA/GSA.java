27d4a7e5d484091c4ad9be1fdad4ce7e9ef855d5
lukacvetkovic <l.cvetk@gmail.com>
2014-11-04 20:27:39 +0100

    Napravljena lista produkcija

--- GSA/GSA.java
+++ GSA/GSA.java
@@ -20,7 +20,7 @@ public class GSA {
 	static Automat eNKA = new Automat();
 	static Definator definator = new Definator();
 	static String line;
-	
+
 	static HashMap<String, List<String>> zapocinjeMap = new HashMap<String, List<String>>();
 
 	static List<GramatickaProdukcija> listaGramtickihProdukcija = new LinkedList<GramatickaProdukcija>();
@@ -97,20 +97,21 @@ public class GSA {
 			}
 			System.out.println();
 		}
-		
+
 		izracunajSkupoveZapocinje();
-		
+
 		System.out.println("#");
-		for(GramatickaProdukcija gramatickaProdukcija : listaGramtickihProdukcija){
+		for (GramatickaProdukcija gramatickaProdukcija : listaGramtickihProdukcija) {
 			System.out.print(gramatickaProdukcija.getLijevaStrana() + " -> ");
-			System.out.println(zapocinjeMap.get(gramatickaProdukcija.getLijevaStrana()));
+			System.out.println(zapocinjeMap.get(gramatickaProdukcija
+					.getLijevaStrana()));
 		}
 		System.out.println("#");
-		
+
 		izGramatickihProdukcijaNapraviProdukcije();
 		System.out.println();
 		System.out.println();
-		
+
 		ispisiProdukcije();
 
 		try {
@@ -122,53 +123,27 @@ public class GSA {
 	}
 
 	private static void ispisiProdukcije() {
-		for(Produkcija prod:listaProdukcija){
+		for (Produkcija prod : listaProdukcija) {
 			prod.ispisi();
 		}
-		
+
 	}
 
 	private static void izGramatickihProdukcijaNapraviProdukcije() {
 
-		for (GramatickaProdukcija produkcija : listaGramtickihProdukcija) {
+		for (GramatickaProdukcija gramatickaProdukcija : listaGramtickihProdukcija) {
 
-			String lijevo = produkcija.getLijevaStrana();
+			Produkcija novaProdukcija = new Produkcija(gramatickaProdukcija);
 
-			for (int i = 0; i <= produkcija.getDesnaStrana().size(); i++) {
-				Produkcija novaProdukcija = new Produkcija(lijevo);
-				if (i == 0) {
-					novaProdukcija.setLjevoOdTockice(null);
-					novaProdukcija.setDesnoOdTockice(produkcija
-							.getDesnaStrana());
-					listaProdukcija.add(novaProdukcija);
+			listaProdukcija.add(novaProdukcija);
 
-				} else if (i == produkcija.getDesnaStrana().size()) {
-					novaProdukcija.setLjevoOdTockice(produkcija
-							.getDesnaStrana());
-					novaProdukcija.setDesnoOdTockice(null);
-					listaProdukcija.add(novaProdukcija);
+			Produkcija iterator = novaProdukcija.createNextProdukcija();
 
-				} else {
-					List<String> pomList = new LinkedList<String>();
-					for (int j = 0; j < i; j++) {
-						pomList.add(produkcija.getDesnaStrana().get(j));
-
-					}
-					novaProdukcija.setLjevoOdTockice(pomList);
-
-					pomList.clear();
-
-					for (int j = i; j < produkcija.getDesnaStrana().size(); j++) {
-						pomList.add(produkcija.getDesnaStrana().get(j));
-
-					}
-					novaProdukcija.setDesnoOdTockice(pomList);
-
-					listaProdukcija.add(novaProdukcija);
-
-				}
-
+			while (iterator != null) {
+				listaProdukcija.add(iterator);
+				iterator = iterator.createNextProdukcija();
 			}
+
 		}
 
 	}
@@ -186,95 +161,118 @@ public class GSA {
 		}
 		return -1;
 	}
-	
-	private static void izracunajSkupoveZapocinje(){
+
+	private static void izracunajSkupoveZapocinje() {
 		List<String> produkcijeSaEpsilonom = new ArrayList<String>();
-		
+
 		Map<String, Set<String>> pomocnaMapa = new HashMap<String, Set<String>>();
-		
-		//Potrpam u listu one sa epsilon prijalzima
-		for(GramatickaProdukcija gramatickaProdukcija : listaGramtickihProdukcija){
+
+		// Potrpam u listu one sa epsilon prijalzima
+		for (GramatickaProdukcija gramatickaProdukcija : listaGramtickihProdukcija) {
 			List<String> desneStrane = gramatickaProdukcija.getDesnaStrana();
-			for(String desnaStrana : desneStrane){
-				if((Arrays.asList(desnaStrana.split("\\s+"))).contains("$")){
-					produkcijeSaEpsilonom.add(gramatickaProdukcija.getLijevaStrana());
+			for (String desnaStrana : desneStrane) {
+				if ((Arrays.asList(desnaStrana.split("\\s+"))).contains("$")) {
+					produkcijeSaEpsilonom.add(gramatickaProdukcija
+							.getLijevaStrana());
 				}
 			}
-		}		
-		
+		}
+
 		// Ubacim sve osim epsilone u kam ide.. cak i nezavrsne znakove
-		for(GramatickaProdukcija gramatickaProdukcija : listaGramtickihProdukcija){
+		for (GramatickaProdukcija gramatickaProdukcija : listaGramtickihProdukcija) {
 			Set<String> zapocinje = new HashSet<String>();
 			List<String> desneStrane = gramatickaProdukcija.getDesnaStrana();
-			for(String desnaStrana : desneStrane){
+			for (String desnaStrana : desneStrane) {
 				String[] znakoviDesneStrane = desnaStrana.split("\\s+");
-				for(int i=0; i<znakoviDesneStrane.length; i++){
-					if(definator.getZavrsniZnakovi().contains(znakoviDesneStrane[i])){
+				for (int i = 0; i < znakoviDesneStrane.length; i++) {
+					if (definator.getZavrsniZnakovi().contains(
+							znakoviDesneStrane[i])) {
 						zapocinje.add(znakoviDesneStrane[i]);
 						break;
 					}
-					if(i==0 && definator.getNezavrsniZnakovi().contains(znakoviDesneStrane[i])){
+					if (i == 0
+							&& definator.getNezavrsniZnakovi().contains(
+									znakoviDesneStrane[i])) {
 						zapocinje.add(znakoviDesneStrane[i]);
 					}
-					if(i>0 && definator.getNezavrsniZnakovi().contains(znakoviDesneStrane[i]) &&
-							produkcijeSaEpsilonom.contains(znakoviDesneStrane[i-1])){
+					if (i > 0
+							&& definator.getNezavrsniZnakovi().contains(
+									znakoviDesneStrane[i])
+							&& produkcijeSaEpsilonom
+									.contains(znakoviDesneStrane[i - 1])) {
 						zapocinje.add(znakoviDesneStrane[i]);
 					}
-					if(!produkcijeSaEpsilonom.contains(znakoviDesneStrane[i])){
+					if (!produkcijeSaEpsilonom.contains(znakoviDesneStrane[i])) {
 						break;
 					}
-					
+
 				}
-				if(znakoviDesneStrane.length != 0 && !znakoviDesneStrane[0].equals("$")){
+				if (znakoviDesneStrane.length != 0
+						&& !znakoviDesneStrane[0].equals("$")) {
 					zapocinje.add(znakoviDesneStrane[0]);
 				}
 			}
 			pomocnaMapa.put(gramatickaProdukcija.getLijevaStrana(), zapocinje);
 		}
-		
-		boolean podesi = true;		// mijenja sve nezavrsne sa njegovom desnom stranom dok ne nestanu svi nezavrsni
-		while(podesi){
+
+		boolean podesi = true; // mijenja sve nezavrsne sa njegovom desnom
+								// stranom dok ne nestanu svi nezavrsni
+		while (podesi) {
 			podesi = false;
-			for(GramatickaProdukcija gramatickaProdukcija : listaGramtickihProdukcija){
-				Set<String> zapocinje = pomocnaMapa.get(gramatickaProdukcija.getLijevaStrana());
-				zapocinje.remove(gramatickaProdukcija.getLijevaStrana());		// micem <A> -> <A> da nebude beskonacno
-				pomocnaMapa.put(gramatickaProdukcija.getLijevaStrana(), zapocinje);
+			for (GramatickaProdukcija gramatickaProdukcija : listaGramtickihProdukcija) {
+				Set<String> zapocinje = pomocnaMapa.get(gramatickaProdukcija
+						.getLijevaStrana());
+				zapocinje.remove(gramatickaProdukcija.getLijevaStrana()); // micem
+																			// <A>
+																			// ->
+																			// <A>
+																			// da
+																			// nebude
+																			// beskonacno
+				pomocnaMapa.put(gramatickaProdukcija.getLijevaStrana(),
+						zapocinje);
 			}
-			for(GramatickaProdukcija gramatickaProdukcija : listaGramtickihProdukcija){
-				Set<String> zapocinje = pomocnaMapa.get(gramatickaProdukcija.getLijevaStrana());
-				for(GramatickaProdukcija gramatickaProdukcija2 : listaGramtickihProdukcija){
-					if(zapocinje.contains(gramatickaProdukcija2.getLijevaStrana())){
+			for (GramatickaProdukcija gramatickaProdukcija : listaGramtickihProdukcija) {
+				Set<String> zapocinje = pomocnaMapa.get(gramatickaProdukcija
+						.getLijevaStrana());
+				for (GramatickaProdukcija gramatickaProdukcija2 : listaGramtickihProdukcija) {
+					if (zapocinje.contains(gramatickaProdukcija2
+							.getLijevaStrana())) {
 						podesi = true;
-						zapocinje.remove(gramatickaProdukcija2.getLijevaStrana());
-						zapocinje.addAll(pomocnaMapa.get(gramatickaProdukcija2.getLijevaStrana()));
+						zapocinje.remove(gramatickaProdukcija2
+								.getLijevaStrana());
+						zapocinje.addAll(pomocnaMapa.get(gramatickaProdukcija2
+								.getLijevaStrana()));
 					}
 				}
-				pomocnaMapa.put(gramatickaProdukcija.getLijevaStrana(), zapocinje);
+				pomocnaMapa.put(gramatickaProdukcija.getLijevaStrana(),
+						zapocinje);
 			}
-			
+
 		}
-		
-		for(GramatickaProdukcija gramatickaProdukcija : listaGramtickihProdukcija){
+
+		for (GramatickaProdukcija gramatickaProdukcija : listaGramtickihProdukcija) {
 			List<String> lista = new ArrayList<String>();
 			lista.addAll(pomocnaMapa.get(gramatickaProdukcija.getLijevaStrana()));
 			zapocinjeMap.put(gramatickaProdukcija.getLijevaStrana(), lista);
-		}				
-		
+		}
+
 	}
-	
-	private static void AutomatUTablice(Automat DKA) throws IOException, JAXBException{
-		
+
+	private static void AutomatUTablice(Automat DKA) throws IOException,
+			JAXBException {
+
 		Tablica akcija = new Tablica();
 		Tablica novoStanje = new Tablica();
-		
-		akcija.setPocetno(DKA.getPocetnoStanje().getImeStanja());		
-		
+
+		akcija.setPocetno(DKA.getPocetnoStanje().getImeStanja());
+
 		FileWriter fw = new FileWriter("analizator/Akcija.xml");
 		JAXBContext context = JAXBContext.newInstance(Tablica.class);
 		Marshaller um = context.createMarshaller();
 		um.marshal(akcija, fw);
 		fw.close();
-		
+
 		fw = new FileWriter("analizator/NovoStanje.xml");
 		context = JAXBContext.newInstance(Tablica.class);
 		um = context.createMarshaller();
