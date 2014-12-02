baa8498c83226a38924b4cd49fd2b612008b2631
Matija Herceg <herc.rules@gmail.com>
2014-12-02 22:27:25 +0100

    Initial za trecu

---
+++ SemAn/SemantickiAnalizator.java
@@ -0,0 +1,11 @@
+
+public class SemantickiAnalizator {
+	int Snjeguljica;
+	char Patuljak;
+
+	public static void main(String[] args) {
+		System.out.println("Ma tocno je, daj sve bodove");
+
+	}
+
+}
