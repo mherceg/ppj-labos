ed2627a760a3e9d79f02f01a7f59d6296a53783f
Mihael Francekovic <mihael.francekovic@fer.hr>
2014-12-05 16:56:54 +0100

    izraz_pridruzivanja - pending TODOs.. a taman me krenulo bez TODOova :(

--- SemAn/izraz_pridruzivanja.java
+++ SemAn/izraz_pridruzivanja.java
@@ -5,7 +5,6 @@ public class izraz_pridruzivanja extends Node {
 	public izraz_pridruzivanja(String name, boolean lIzraz, List<Tip> type,
 			int red, int brElem) {
 		super(name, lIzraz, type, red, brElem);
-
 	}
 
 	/**
@@ -13,8 +12,23 @@ public class izraz_pridruzivanja extends Node {
 	 */
 	@Override
 	public void provjeri() {
-		// TODO Auto-generated method stub
-
+		Node childNula = child.get(0);
+		if(childNula.getName().equals("<"+log_ili_izraz.class.getName()+">")){
+			childNula.provjeri();
+			this.characteristics.setType(childNula.getType());
+		}
+		else if(childNula.getName().equals("<"+postfiks_izraz.class.getName()+">")){
+			Node childDva = child.get(2);
+			childNula.provjeri();
+			// TODO <postfiks_izraz>.l-izraz = 1
+			childDva.provjeri();
+			// TODO <izraz_pridruzivanja>.tip  <postfiks_izraz>.tip
+			this.characteristics.setType(childNula.getType());
+			this.characteristics.setlIzraz(false);
+		}		
+		else{
+			System.err.println("Greska kod " + this.getClass().getName() + " za -> " + child.toString());
+		}
 	}
 
 }
