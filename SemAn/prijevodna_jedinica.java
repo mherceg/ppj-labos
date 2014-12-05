b9dde0da38e7c3c33e39487a3a0eef3b1c58c275
Mihael Francekovic <mihael.francekovic@fer.hr>
2014-12-05 16:10:47 +0100

    prijevodna_jedinica

--- SemAn/prijevodna_jedinica.java
+++ SemAn/prijevodna_jedinica.java
@@ -12,7 +12,18 @@ public class prijevodna_jedinica extends Node {
 	 */
 	@Override
 	public void provjeri() {
-		// TODO Auto-generated method stub
+		Node childNula = child.get(0);
+		if(childNula.getName().equals("<"+vanjska_deklaracija.class.getName()+">")){
+			childNula.provjeri();
+		}
+		else if(childNula.getName().equals("<"+prijevodna_jedinica.class.getName()+">")){
+			Node childJedan = child.get(1);
+			childNula.provjeri();
+			childJedan.provjeri();
+		}		
+		else{
+			System.err.println("Greska kod " + this.getClass().getName() + " za -> " + child.toString());
+		}
 
 	}
 
