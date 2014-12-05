276a64a929721cd20689e5edb091c64ac10e0a1d
Mihael Francekovic <mihael.francekovic@fer.hr>
2014-12-05 16:28:38 +0100

    naredba_grananja - pending TODOs

--- SemAn/naredba_grananja.java
+++ SemAn/naredba_grananja.java
@@ -4,15 +4,32 @@ public class naredba_grananja extends Node {
 	
 	public naredba_grananja(String name, boolean lIzraz, List<Tip> type, int red, int brElem) {
 		super(name, lIzraz, type, red, brElem);
-
 	}
 	/**
 	 * Str 63f
 	 */
 	@Override
 	public void provjeri() {
-		// TODO Auto-generated method stub
-
+		int childCount = child.size();
+		if(childCount == 5){
+			Node childDva = child.get(2);
+			Node childCetri = child.get(4);
+			childDva.provjeri();
+			// TODO 2. <izraz>.tip tilda int
+			childCetri.provjeri();
+		}
+		else if(childCount == 7){
+			Node childDva = child.get(2);
+			Node childCetri = child.get(4);
+			Node childSest = child.get(6);
+			childDva.provjeri();
+			// TODO 2. <izraz>.tip tilda int
+			childCetri.provjeri();
+			childSest.provjeri();
+		}		
+		else{
+			System.err.println("Greska kod " + this.getClass().getName() + " za -> " + child.toString());
+		}
 	}
 
 }
