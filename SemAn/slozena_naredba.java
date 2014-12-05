1bb0cd297143096d7a07d12276edbb79563c494e
Mihael Francekovic <mihael.francekovic@fer.hr>
2014-12-05 16:49:06 +0100

    slozena_naredba

--- SemAn/slozena_naredba.java
+++ SemAn/slozena_naredba.java
@@ -12,8 +12,18 @@ public class slozena_naredba extends Node{
 	 */
 	@Override
 	public void provjeri() {
-		// TODO Auto-generated method stub
-		
+		Node childJedan = child.get(1);
+		if(childJedan.getName().equals("<"+lista_naredbi.class.getName()+">")){
+			childJedan.provjeri();
+		}
+		else if(childJedan.getName().equals("<"+lista_deklaracija.class.getName()+">")){
+			Node childDva = child.get(2);
+			childJedan.provjeri();
+			childDva.provjeri();
+		}		
+		else{
+			System.err.println("Greska kod " + this.getClass().getName() + " za -> " + child.toString());
+		}
 	}
 
 }
