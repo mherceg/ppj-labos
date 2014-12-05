d43bd546ceac5a5d85d1331c40e98f01000c5186
Adriano Bacac <adrianobacac@gmail.com>
2014-12-05 16:14:51 +0100

    definirao pomocne funkcije, dodao get u memoriju

--- SemAn/Provjerinator.java
+++ SemAn/Provjerinator.java
@@ -1,45 +1,77 @@
 public class Provjerinator {
+	/**
+	 * pravila za raspon CHAR, str 40, Brojevni tipovi
+	 * @param value
+	 * @return
+	 */
 	
-	public static boolean rasponChar(String value){
+	public static boolean rasponChar(String value) {
 		int charValue;
 		try {
-			charValue=Integer.parseInt(value);
+			charValue = Integer.parseInt(value);
 		} catch (Exception e) {
 			return false;
 		}
-		if(charValue<0|| charValue>255){
+		if (charValue < 0 || charValue > 255) {
 			return false;
 		}
 		return true;
 	}
-	
+	/**
+	 * ravila za raspon INT, str 40, Brojevni tipovi
+	 * @param value
+	 * @return
+	 */
 	public static boolean rasponInt(String value) {
 		int intValue;
 		try {
-			intValue=Integer.parseInt(value);
+			intValue = Integer.parseInt(value);
 		} catch (Exception e) {
 			return false;
 		}
-		if(intValue<-2147483648 || intValue>2147483647){
+		if (intValue < -2147483648 || intValue > 2147483647) {
 			return false;
 		}
 		return true;
 	}
-	
-	public static boolean tilda(Tip a, Tip b){
-		switch (a) {
-		case null: //TODO
-			
-			break;
-
-		default:
-			break;
-		}
-		
-		
-		return false;
-		
+	/**
+	 * Pravila za ZNAK, str 42, 4.3.2
+	 * @param znak
+	 * @return
+	 */
+	public static boolean znakOK(String znak) {
+		//TODO
+		return true;
 	}
-	
-	
+
+	/**
+	 * Pravila za NIZ_ZNAKOVA, str 42, 4.3.2
+	 * @param znak
+	 * @return
+	 */
+	public static boolean konstNizZnakovaOK(String niz) {
+		//TODO
+		return true;
+	}
+	/**
+	 * a se moze implicitno pretvorit u b, str41, Implicitne promjene tipa
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static boolean tilda(Tip a, Tip b) {
+		//TODO
+		return false;
+
+	}
+	/**
+	 * a se moze kastat u b, str 42, Eksplicitne promjene tipa
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static boolean isCastable(Tip a, Tip b){
+		//TODO
+		return false;
+	}
 }
--- SemAn/VariableMemory.java
+++ SemAn/VariableMemory.java
@@ -1,52 +1,66 @@
 import java.util.HashMap;
 
-
 public class VariableMemory {
-	
+
 	private HashMap<String, Characteristics> hm;
-	
+
 	private VariableMemory previous;
 	private VariableMemory next;
-	
-	private VariableMemory current; 
-	
+
+	private VariableMemory current;
+
 	public VariableMemory() {
 		this.hm = new HashMap<String, Characteristics>();
-		this.current=this;
+		this.current = this;
 	}
-	
-	public void goDown(){
+
+	public void goDown() {
 		this.next = new VariableMemory();
 		this.next.previous = this;
 		this.current = this.next;
 	}
-	public void goUp(){
+
+	public void goUp() {
 		this.current = this.current.previous;
 	}
-	public boolean containsAtThisLevel(String name){
-		if(current.hm.containsKey(name)){
+
+	public boolean containsAtThisLevel(String name) {
+		if (current.hm.containsKey(name)) {
 			return true;
 		}
 		return false;
 	}
-	public boolean contains(String name){
+
+	public boolean contains(String name) {
 		VariableMemory iter = current;
-		if(iter.containsAtThisLevel(name)){
-			return true;
-		}
-		while(iter!=null){
-			iter = current.previous;
-			if(iter.containsAtThisLevel(name)){
+		while (iter != null) {
+			if (iter.containsAtThisLevel(name)) {
 				return true;
 			}
+			iter = iter.previous;
+			
 		}
 		return false;
 	}
-	public boolean add(String name, Characteristics value){
-		if(current.containsAtThisLevel(name)){
+
+	public boolean add(String name, Characteristics value) {
+		if (current.containsAtThisLevel(name)) {
 			return false;
 		}
 		current.hm.put(name, value);
 		return true;
+	}
+
+	public Characteristics get(String name) {
+		Characteristics ret = null;
+		VariableMemory iter = current;
+		while (iter != null) {
+			if ((ret = iter.hm.get(name)) != null) {
+				return ret;
+			}
+			iter=iter.previous;
+		}
+		return null;
+
 	}
 }
