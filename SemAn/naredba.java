9466103f7b3568992c9edb901d26123caa8888ec
Mihael Francekovic <mihael.francekovic@fer.hr>
2014-12-05 16:43:49 +0100

    naredba - jedna bez pending TODOa :D

--- SemAn/naredba.java
+++ SemAn/naredba.java
@@ -11,7 +11,25 @@ public class naredba extends Node {
 	 */
 	@Override
 	public void provjeri() {
-		// TODO Auto-generated method stub
+		Node childNula = child.get(0);
+		if(childNula.getName().equals("<"+slozena_naredba.class.getName()+">")){
+			childNula.provjeri();
+		}
+		else if(childNula.getName().equals("<"+izraz_naredba.class.getName()+">")){
+			childNula.provjeri();
+		}	
+		else if(childNula.getName().equals("<"+naredba_grananja.class.getName()+">")){
+			childNula.provjeri();
+		}	
+		else if(childNula.getName().equals("<"+naredba_petlje.class.getName()+">")){
+			childNula.provjeri();
+		}	
+		else if(childNula.getName().equals("<"+naredba_skoka.class.getName()+">")){
+			childNula.provjeri();
+		}	
+		else{
+			System.err.println("Greska kod " + this.getClass().getName() + " za -> " + child.toString());
+		}
 
 	}
 
