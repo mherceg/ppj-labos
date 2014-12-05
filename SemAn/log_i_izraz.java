83e0a184c5ab8e41c2f04a199d8ca9fce75f4d11
Mihael Francekovic <mihael.francekovic@fer.hr>
2014-12-05 17:12:14 +0100

    log_i_izraz - pending TODOs

--- SemAn/log_i_izraz.java
+++ SemAn/log_i_izraz.java
@@ -12,8 +12,24 @@ public class log_i_izraz extends Node {
 	 */
 	@Override
 	public void provjeri() {
-		// TODO Auto-generated method stub
-
+		Node childNula = child.get(0);
+		if(childNula.getName().equals("<"+bin_ili_izraz.class.getName()+">")){
+			childNula.provjeri();
+			this.characteristics.setType(childNula.getType());
+			this.characteristics.setlIzraz(childNula.getlIzraz());
+		}
+		else if(childNula.getName().equals("<"+log_i_izraz.class.getName()+">")){
+			Node childDva = child.get(2);
+			childNula.provjeri();
+			// TODO 2. <log_i_izraz>.tip  int			
+			childDva.provjeri();
+			// TODO 4. <bin_ili_izraz>.tip  int
+			// TODO this.characteristics.setType(Tip.int);
+			this.characteristics.setlIzraz(false);
+		}		
+		else{
+			System.err.println("Greska kod " + this.getClass().getName() + " za -> " + child.toString());
+		}
 	}
 
 }
