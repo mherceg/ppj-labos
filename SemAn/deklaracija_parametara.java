85fe18a0966e896a11f6d584d6ea4e1ffc34acef
Mihael Francekovic <mihael.francekovic@fer.hr>
2014-12-05 15:50:00 +0100

    deklaracija_parametara - pending TODOs

--- SemAn/deklaracija_parametara.java
+++ SemAn/deklaracija_parametara.java
@@ -1,3 +1,4 @@
+import java.awt.Window.Type;
 import java.util.List;
 
 public class deklaracija_parametara extends Node {
@@ -13,7 +14,23 @@ public class deklaracija_parametara extends Node {
 	 */
 	@Override
 	public void provjeri() {
-		// TODO Auto-generated method stub
+		int childCount = child.size();
+		Node childNula = child.get(0);
+		if(childCount == 2){
+			childNula.provjeri();
+			// TODO <ime_tipa>.tim != void .... childNula.getType != Tip.void
+			this.characteristics.setType(childNula.getType());
+			// TODO this.characteristics.setName(IDN.name);
+		}
+		else if(childCount == 4){
+			childNula.provjeri();
+			// TODO <ime_tipa>.tim != void .... childNula.getType != Tip.void
+			// TODO this.characteristics.setType(niz(childNula.getType()));
+			// TODO this.characteristics.setName(IDN.name); 
+		}	
+		else{
+			System.err.println("Greska kod " + this.getClass().getName() + " za -> " + child.toString());
+		}
 
 	}
 
