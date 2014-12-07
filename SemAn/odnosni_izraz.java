9e591500450eaa8acc729ed67d04f082cc4102e9
Mihael Francekovic <mihael.francekovic@fer.hr>
2014-12-07 02:11:55 +0100

    odnosni_izraz - pending TODOs

--- SemAn/odnosni_izraz.java
+++ SemAn/odnosni_izraz.java
@@ -9,7 +9,25 @@ public class odnosni_izraz extends Node{
 
 	@Override
 	public void provjeri() {
-		// TODO Auto-generated method stub
-		
+		Node childNula = child.get(0);
+		if(childNula.getName().equals("<"+aditivni_izraz.class.getName()+">")){
+			childNula.provjeri();
+			this.characteristics.setType(childNula.getType());
+			this.characteristics.setlIzraz(childNula.getlIzraz());
+		}
+		// TODO ovdje je linija na ulazu <odnosni_izraz> (OP_LT | OP_GT | OP_LTE | OP_GTE) <aditivni_izraz>
+		// Dal ej zagreda jedan child ali neki iz te skupine?
+		else if(childNula.getName().equals("<"+odnosni_izraz.class.getName()+">")){
+			Node childDva = child.get(2);
+			childNula.provjeri();
+			// TODO 2. <odnosni_izraz>.tip  int
+			childDva.provjeri();
+			// TODO 4. <aditivni_izraz>.tip  int
+			// TODO this.characteristics.setType(Tip.int);
+			this.characteristics.setlIzraz(false);
+		}		
+		else{
+			System.err.println("Greska kod " + this.getClass().getName() + " za -> " + child.toString());
+		}
 	}	
 }
