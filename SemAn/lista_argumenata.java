419c802f169bbeb14e42ad92c74114d793d3f5b1
Mihael Francekovic <mihael.francekovic@fer.hr>
2014-12-07 02:36:02 +0100

    lista_argumenata

--- SemAn/lista_argumenata.java
+++ SemAn/lista_argumenata.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class lista_argumenata extends Node {
@@ -12,8 +13,22 @@ public class lista_argumenata extends Node {
 	 */
 	@Override
 	public void provjeri() {
-		// TODO Auto-generated method stub
-
+		Node childNula = child.get(0);
+		if(childNula.getName().equals("<"+izraz_pridruzivanja.class.getName()+">")){
+			childNula.provjeri();
+			this.characteristics.setType(childNula.getType());
+		}
+		else if(childNula.getName().equals("<"+lista_argumenata.class.getName()+">")){
+			Node childJedan = child.get(1);
+			childNula.provjeri();
+			childJedan.provjeri();
+			List<Tip> tipovi = new ArrayList<Tip>(childNula.getType());
+			tipovi.addAll(childJedan.getType());
+			this.characteristics.setType(tipovi);
+		}		
+		else{
+			System.err.println("Greska kod " + this.getClass().getName() + " za -> " + child.toString());
+		}
 	}
 
 }
