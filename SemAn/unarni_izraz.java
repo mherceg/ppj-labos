d6842af701980ee88e8b17b48f18e34a947c64bb
Mihael Francekovic <mihael.francekovic@fer.hr>
2014-12-07 02:32:35 +0100

    unarni_izraz - pending TODOs

--- SemAn/unarni_izraz.java
+++ SemAn/unarni_izraz.java
@@ -11,8 +11,34 @@ public class unarni_izraz extends Node {
 	 */
 	@Override
 	public void provjeri() {
-		// TODO Auto-generated method stub
-
+		Node childNula = child.get(0);
+		if(childNula.getName().equals("<"+postfiks_izraz.class.getName()+">")){
+			childNula.provjeri();
+			this.characteristics.setType(childNula.getType());
+			this.characteristics.setlIzraz(childNula.getlIzraz());
+		}
+		else if(childNula.getName().equals("<"+unarni_operator.class.getName()+">")){
+			Node childJedan = child.get(1);
+			childJedan.provjeri();
+			// TODO 2. <cast_izraz>.tip  int
+			// TODO this.characteristics.setType(Tip.int);
+			// TODO this.characteristics.setlIzraz(false);
+		}
+		else if(child.size() == 2){
+			if(childNula.getName().equals("<"+unarni_izraz.class.getName()+">")){
+				Node childJedan = child.get(1);
+				childJedan.provjeri();
+				// 2. <unarni_izraz>.l-izraz = 1 i <unarni_izraz>.tip  int
+				// TODO this.characteristics.setType(Tip.int);
+				// TODO this.characteristics.setlIzraz(false);
+			}
+			else{
+				System.err.println("Greska kod " + this.getClass().getName() + " za -> " + child.toString());
+			}
+		}
+		else{
+			System.err.println("Greska kod " + this.getClass().getName() + " za -> " + child.toString());
+		}
 	}
 
 }
