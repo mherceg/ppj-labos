1ba8b4bbcf20954291a22363c0a628d86257bd04
Mihael Francekovic <mihael.francekovic@fer.hr>
2014-12-07 02:00:35 +0100

    bin_ili_izraz - pending TODOs

--- SemAn/bin_ili_izraz.java
+++ SemAn/bin_ili_izraz.java
@@ -11,8 +11,24 @@ public class bin_ili_izraz extends Node {
 	 */
 	@Override
 	public void provjeri() {
-		// TODO Auto-generated method stub
-
+		Node childNula = child.get(0);
+		if(childNula.getName().equals("<"+bin_xili_izraz.class.getName()+">")){
+			childNula.provjeri();
+			this.characteristics.setType(childNula.getType());
+			this.characteristics.setlIzraz(childNula.getlIzraz());
+		}
+		else if(childNula.getName().equals("<"+bin_ili_izraz.class.getName()+">")){
+			Node childDva = child.get(2);
+			childNula.provjeri();
+			// TODO 2. <bin_ili_izraz>.tip  int
+			childDva.provjeri();
+			// TODO 4. <bin_xili_izraz>.tip  int
+			// TODO this.characteristics.setType(Tip.int);
+			this.characteristics.setlIzraz(false);
+		}		
+		else{
+			System.err.println("Greska kod " + this.getClass().getName() + " za -> " + child.toString());
+		}
 	}
 
 }
