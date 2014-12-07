bb47f6030e6517d1fb6ed68b9f2c726e2edb5791
Mihael Francekovic <mihael.francekovic@fer.hr>
2014-12-07 02:23:38 +0100

    ime_tipa - pending TODOs

--- SemAn/ime_tipa.java
+++ SemAn/ime_tipa.java
@@ -12,8 +12,20 @@ public class ime_tipa extends Node {
 	 */
 	@Override
 	public void provjeri() {
-		// TODO Auto-generated method stub
-
+		if(child.size() == 1){
+			Node childNula = child.get(0);
+			childNula.provjeri();
+			this.characteristics.setType(childNula.getType());
+		}
+		else if(child.size() == 2){
+			Node childJedan = child.get(1);
+			childJedan.provjeri();
+			this.characteristics.setType(childJedan.getType());
+			// TODO 2. <specifikator_tipa>.tip != void
+		}		
+		else{
+			System.err.println("Greska kod " + this.getClass().getName() + " za -> " + child.toString());
+		}
 	}
 
 }
