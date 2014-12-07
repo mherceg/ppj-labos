20d479ad5040c157487adbf55dd78e32bf4d39ad
Mihael Francekovic <mihael.francekovic@fer.hr>
2014-12-07 02:16:08 +0100

    multiplikativni_izraz - pending TODOs

--- SemAn/multiplikativni_izraz.java
+++ SemAn/multiplikativni_izraz.java
@@ -12,8 +12,24 @@ public class multiplikativni_izraz extends Node{
 	 */
 	@Override
 	public void provjeri() {
-		// TODO Auto-generated method stub
-		
+		Node childNula = child.get(0);
+		if(childNula.getName().equals("<"+cast_izraz.class.getName()+">")){
+			childNula.provjeri();
+			this.characteristics.setType(childNula.getType());
+			this.characteristics.setlIzraz(childNula.getlIzraz());
+		}
+		else if(childNula.getName().equals("<"+multiplikativni_izraz.class.getName()+">")){
+			Node childDva = child.get(2);
+			childNula.provjeri();
+			// TODO 2. <mutliplikativni_izraz>.tip  int
+			childDva.provjeri();
+			// TODO 4. <cast_izraz>.tip  int
+			// TODO this.characteristics.setType(Tip.int);
+			this.characteristics.setlIzraz(false);
+		}		
+		else{
+			System.err.println("Greska kod " + this.getClass().getName() + " za -> " + child.toString());
+		}
 	}
 	
 
