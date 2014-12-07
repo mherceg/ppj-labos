0b13bf2427999c4bbadd71da0798f9672de4a779
Mihael Francekovic <mihael.francekovic@fer.hr>
2014-12-07 02:14:42 +0100

    aditivni_izraz - pending TODOs

--- SemAn/aditivni_izraz.java
+++ SemAn/aditivni_izraz.java
@@ -11,8 +11,24 @@ public class aditivni_izraz extends Node {
 	 */
 	@Override
 	public void provjeri() {
-		// TODO Auto-generated method stub
-
+		Node childNula = child.get(0);
+		if(childNula.getName().equals("<"+multiplikativni_izraz.class.getName()+">")){
+			childNula.provjeri();
+			this.characteristics.setType(childNula.getType());
+			this.characteristics.setlIzraz(childNula.getlIzraz());
+		}
+		else if(childNula.getName().equals("<"+aditivni_izraz.class.getName()+">")){
+			Node childDva = child.get(2);
+			childNula.provjeri();
+			// TODO 2. <aditivni_izraz>.tip  int
+			childDva.provjeri();
+			// TODO 4. <multiplikativni_izraz>.tip  int
+			// TODO this.characteristics.setType(Tip.int);
+			this.characteristics.setlIzraz(false);
+		}		
+		else{
+			System.err.println("Greska kod " + this.getClass().getName() + " za -> " + child.toString());
+		}
 	}
 
 }
